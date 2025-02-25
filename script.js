7948141fc27d801ea0988c266ae2cccb186fc419
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-02-25 12:16:20 +0100

    reshape the article

--- script.js
+++ script.js
@@ -1,50 +1,31 @@
-const circle_a = document.querySelector("#btn-wire-a .slide-circle");
-const circle_b = document.querySelector("#btn-wire-b .slide-circle");
-const button_a = document.querySelector("#btn-wire-a");
-const wire_a = document.querySelector("#wire-a");
-const wire_b = document.querySelector("#wire-b");
-const wire_c = document.querySelector("#wire-c");
+const btn_a = document.querySelector('#nand-gate-a-btn');
+const btn_b = document.querySelector('#nand-gate-b-btn');
+const wire_a = document.querySelector('#wire-a');
+const wire_b = document.querySelector('#wire-b');
+const wire_c = document.querySelector('#wire-c');
 
-let a_toLeft = false;
-let b_toLeft = false;
+let a_off = false;
+let b_off = false;
+[btn_a, btn_b].forEach(btn => {
+	btn.addEventListener('click', () => {
+		if (btn === btn_a) {
+			a_off
+			? (wire_a.style.borderColor = 'goldenrod')
+			: (wire_a.style.borderColor = 'yellow');
+		} else {
+			b_off
+			? (wire_b.style.borderColor = 'goldenrod')
+			: (wire_b.style.borderColor = 'yellow');
+		}
 
-[circle_a, circle_b].forEach((circle) => {
-  circle.addEventListener("click", (e) => {
-    slideCircle(e.target);
-    changeWireColor(e.target);
+		btn === btn_a ? (a_off = !a_off) : (b_off = !b_off);
+		if (a_off && b_off) {
+			wire_c.style.borderColor = 'goldenrod';
+		} else {
+			wire_c.style.borderColor = 'yellow';
+		}
 
-    circle === circle_a ? (a_toLeft = !a_toLeft) : (b_toLeft = !b_toLeft);
-    if (a_toLeft && b_toLeft) {
-      wire_c.style.backgroundColor = "hsl(29, 57%, 46%)";
-    } else {
-      wire_c.style.backgroundColor = "yellow";
-    }
-  });
+		btn.textContent = btn.textContent === 'off' ? 'on' : 'off';
+	});
 });
 
-const leftDistance = button_a.clientWidth - circle_a.offsetWidth;
-function slideCircle(circle) {
-  if (circle === circle_a) {
-    if (a_toLeft) {
-      circle.style.left = "0";
-    } else {
-      circle.style.left = `${leftDistance}px`;
-    }
-    return;
-  }
-  b_toLeft
-    ? (circle.style.left = "0")
-    : (circle.style.left = `${leftDistance}px`);
-}
-
-function changeWireColor(circle) {
-  if (circle === circle_a) {
-    a_toLeft
-      ? (wire_a.style.backgroundColor = "hsl(29, 57%, 46%)")
-      : (wire_a.style.backgroundColor = "yellow");
-  } else {
-    b_toLeft
-      ? (wire_b.style.backgroundColor = "hsl(29, 57%, 46%)")
-      : (wire_b.style.backgroundColor = "yellow");
-  }
-}
